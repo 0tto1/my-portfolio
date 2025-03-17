b3610c5d5c94744ef2e7c83de1c99265d5f33fdb
0tto1 <miraskadyrbek2@gmail.com>
2025-03-17 13:59:06 +0500

    Delete portfolio directory

--- portfolio/script.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('theme-toggle').addEventListener('click', function() {
-    document.body.classList.toggle('dark-mode');
-    document.body.classList.toggle('light-mode');
-});
